6792ca4c91dee59ead0091738293b7dc8ccedcca
RyanDaSilvaMarinho <rdsm.snf23@uea.edu.br>
2023-11-24 12:34:42 -0400

    branch anterior

--- pongpygame/main.py
+++ pongpygame/main.py
@@ -1,6 +1,5 @@
 import pygame
-import random
-punched_corner = False
+
 pygame.init()
 
 COLOR_BLACK = (0, 0, 0)
@@ -35,20 +34,21 @@ PADDLE_HEIGHT = 150
 
 # player 1
 player_1 = pygame.image.load("assets/player.png")
-player_1_y = 300
+player_1_y = 300  # paddle position
 player_1_move_up = False
 player_1_move_down = False
 
-# player 2 - AI
+# player 2 - robot
 player_2 = pygame.image.load("assets/player.png")
-player_2_y = 300
+player_2_y = 300  # paddle position
 
 # ball
 ball = pygame.image.load("assets/ball.png")
-ball_x = VW / 2
-ball_y = VH / 2
+ball_x = VW / 2  # ball position vw
+ball_y = VH / 2  # ball position vh
 ball_dx = 5
 ball_dy = 5
+punched_corner = False
 
 # score
 score_1 = 0
@@ -59,10 +59,12 @@ game_loop = True
 game_clock = pygame.time.Clock()
 
 while game_loop:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_loop = False
-        # keystroke events
+
+        #  keystroke events
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 player_1_move_up = True
@@ -74,61 +76,71 @@ while game_loop:
             if event.key == pygame.K_DOWN:
                 player_1_move_down = False
 
-    # checking victory condition
+    # checking the victory condition
     if score_1 < SCORE_MAX and score_2 < SCORE_MAX:
+
+        # clear screen
         screen.fill(COLOR_BLACK)
 
         # ball collision with the wall
-        if ball_y >= VH - 20 or ball_y <= 0:
+        if ball_y > 700 or ball_y <= 0:
             ball_dy *= -1
             bounce_sound_effect.play()
+            punched_corner = False
 
-        # ball collision with player 1
+        # ball collision with the player 1 's paddle
         if ball_x < 100:
             if ball_x > 90:
                 # ball touches the paddle
                 if player_1_y < ball_y + (ball_dx * ball_dy):
                     if player_1_y + PADDLE_HEIGHT > ball_y:
                         ball_dx *= -1
-                    bounce_sound_effect.play()
+                        bounce_sound_effect.play()
             elif not punched_corner:
                 if player_1_y < ball_y + (ball_dx * ball_dy):
                     if player_1_y + PADDLE_HEIGHT > ball_y:
                         ball_dy *= -1
-                    bounce_sound_effect.play()
-                    punched_corner = True
+                        bounce_sound_effect.play()
+                        punched_corner = True
 
-        # ball collision with player 2
-        elif ball_x > 1160 and player_2_y < ball_y < player_2_y + PADDLE_HEIGHT:
-            ball_dx *= -1
-            bounce_sound_effect.play()
+        # ball collision with the player 2 's paddle
+        if ball_x > 1160:
+            if player_2_y < ball_y + 25:
+                if player_2_y + PADDLE_HEIGHT > ball_y:
+                    ball_dx *= -1
+                    bounce_sound_effect.play()
 
         # scoring points
         if ball_x < -50:
-            ball_x = VW / 2
-            ball_y = random.uniform(50, VH - 50)
-            ball_dy = random.choice([6.5, -6.5])
+            ball_x = 640
+            ball_y = 360
+            ball_dy *= -1
             ball_dx *= -1
             score_2 += 1
             scoring_sound_effect.play()
-
-        elif ball_x > VW + 50:
-            ball_x = VW / 2
-            ball_y = random.uniform(50, VH - 50)
-            ball_dy = random.choice([6.5, -6.5])
+        elif ball_x > 1320:
+            ball_x = 640
+            ball_y = 360
+            ball_dy *= -1
             ball_dx *= -1
             score_1 += 1
             scoring_sound_effect.play()
 
         # ball movement
-        ball_x += ball_dx
-        ball_y += ball_dy
+        ball_x = ball_x + ball_dx
+        ball_y = ball_y + ball_dy
 
-        # player 1 movement
-        if player_1_move_up and player_1_y > 0:
-            player_1_y -= 5.5
-        elif player_1_move_down and player_1_y < VH - PADDLE_HEIGHT:
-            player_1_y += 5.5
+        # player 1 up movement
+        if player_1_move_up:
+            player_1_y -= 5
+        else:
+            player_1_y += 0
+
+        # player 1 down movement
+        if player_1_move_down:
+            player_1_y += 5
+        else:
+            player_1_y += 0
 
         # player 1 collides with upper wall
         if player_1_y <= 0:
@@ -138,13 +150,13 @@ while game_loop:
         elif player_1_y >= 570:
             player_1_y = 570
 
-        if ball_x > VW / 2:
-            # making AI unpredictable
-            target_y = ball_y + random.randint(-20, 20)
-            if player_2_y + PADDLE_HEIGHT / 2 < target_y and player_2_y < VH - PADDLE_HEIGHT:
-                player_2_y += random.uniform(3, 5)
-            elif player_2_y + PADDLE_HEIGHT / 2 > target_y and player_2_y > 0:
-                player_2_y -= random.uniform(3, 5)
+        # player 2 "Artificial Intelligence"
+        player_2_y = ball_y
+        if player_2_y <= 0:
+            player_2_y = 0
+        elif player_2_y >= 570:
+            player_2_y = 570
+
         # update score hud
         score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
 
